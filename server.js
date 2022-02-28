8d5a95efe3e7f2cdbf60cba46d117e6b156d9809
ckyle121 <carolinekyle21@gmail.com>
2022-02-28 10:02:30 -0600

    CRUD operations for candidate party

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { result } = require('lodash');
 const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
 
@@ -69,28 +70,6 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
-// DELETE a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`
-    const params = [req.params.id]; 
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.statusMessage(400).json({ error: err.message });
-        } else if (!result.affectedRows) {
-            res.json( {
-                message: 'Candidate not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
 // CREATE a candidate
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
@@ -115,8 +94,114 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// UPDATE a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    // candidate party affiliation is null 
+    const errors = inputCheck(req.body, 'party_id');
+    if (errors) {
+        res.status(400).json({ error: errors});
+        return;
+    }
 
-// default response for any other request 
+    const sql = `UPDATE candidates SET party_id = ?
+                WHERE id = ?`
+    
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err){
+            res.status(400).json({ error: err.message});
+        } else if (!result.affectedRows){
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+// DELETE a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`
+    const params = [req.params.id]; 
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json( {
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// GET all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if (err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// GET single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// DELETE a party 
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message});
+        } else if  (!result.affectedRows){
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// Not Found response for unmatched routes
 app.use((req, res) => {
     res.status(404).end();
 });
